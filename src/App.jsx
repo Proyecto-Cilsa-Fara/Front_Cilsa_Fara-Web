fb86901c34b2057743f5e5040a04dfb340124386
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-04 20:19:29 -0300

    Add WIP_MESSAGE and ERROR_MESSAGE for primitive routing implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router"
+
 function App() {
-    return <></>
+    const WIP_MESSAGE = "Página aún en construcción..."
+    const ERROR_MESSAGE = "¡UPS! Esa página no existe..."
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<h2>{WIP_MESSAGE}</h2>} />
+                <Route path="*" element={<h2>{ERROR_MESSAGE}</h2>} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
