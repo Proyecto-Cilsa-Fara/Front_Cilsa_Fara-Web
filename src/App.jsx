9d7c125739dd9a6f7a82408bd03f26ed893cddfe
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-04 21:23:11 -0300

    Replace ERROR_MESSAGE with ErrorPage in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router"
-import { WIPPage } from "./pages"
+import { ErrorPage, WIPPage } from "./pages"
 
 function App() {
-    const ERROR_MESSAGE = "¡UPS! Esa página no existe..."
-
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<WIPPage />} />
-                <Route path="*" element={<h2>{ERROR_MESSAGE}</h2>} />
+                <Route path="*" element={<ErrorPage />} />
             </Routes>
         </BrowserRouter>
     )
