d0e2784c8fac0b10a1f8fb9a402981e69b8452ea
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-04 21:18:47 -0300

    Replace WIP_MESSAGE with WIPPAge in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router"
+import { WIPPage } from "./pages"
 
 function App() {
-    const WIP_MESSAGE = "Página aún en construcción..."
     const ERROR_MESSAGE = "¡UPS! Esa página no existe..."
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<h2>{WIP_MESSAGE}</h2>} />
+                <Route path="/" element={<WIPPage />} />
                 <Route path="*" element={<h2>{ERROR_MESSAGE}</h2>} />
             </Routes>
         </BrowserRouter>
