d7c32988f6a636104bdf3f014d5380912d7c2e09
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-06 17:01:15 -0300

    Add primitive implementation of ColorBar.jsx

--- src/components/ColorBar.jsx
+++ src/components/ColorBar.jsx
@@ -1,3 +1,12 @@
 export function ColorBar() {
-    return <></>
+    /* Color Bar with Diagonal Segments */
+
+    return (
+        <div className="flex h-8 w-full overflow-hidden bg-red-700 shadow-md">
+            <div className="w-1/4 bg-red-700"></div>
+            <div className="w-1/4 skew-x-[-45deg] bg-teal-500"></div>
+            <div className="w-1/4 skew-x-[-45deg] bg-lime-400"></div>
+            <div className="w-1/4 skew-x-[-45deg] bg-amber-500"></div>
+        </div>
+    )
 }
