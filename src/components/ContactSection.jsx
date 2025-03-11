4bc67f0a5b34d1f09581915dcba23ce1fb0e766c
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-11 13:35:49 -0300

    Replace const definitions with contactInfo.js information in ContactSection.jsx

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,13 +1,5 @@
 import { EmailIcon, FacebookIcon, InstagramIcon, LinkedinIcon, WhatsappIcon } from "../assets/icons"
-
-// Datos de contacto de la organización (Constantes para mejor legibilidad y mantenimiento):
-
-const NUMERO = "+54 9 2215043359"
-const EMAIL1 = "accionrestaurativapaz@gmail.com"
-const EMAIL2 = "accionrestaurativa@hotmail.com"
-const FACEBOOK = "@faceaccionrestaurativa"
-const INSTAGRAM = "@fara.accionrestaurativa"
-const LINKEDIN = "Acción Restaurativa Argentina"
+import { CONTACT_INFO } from "../utils/contactInfo"
 
 export function ContactSection() {
     /* Seccion de contacto */
@@ -22,19 +14,19 @@ export function ContactSection() {
                         <div className="rounded-lg bg-white p-1">
                             <img src={WhatsappIcon} className="text-3xl text-red-700" />
                         </div>
-                        <span className="text-lg font-semibold">{NUMERO}</span>
+                        <span className="text-lg font-semibold">{CONTACT_INFO.PHONE}</span>
                     </li>
                     <li className="flex items-center gap-3">
                         <div className="rounded-lg bg-white p-1">
                             <img src={EmailIcon} className="text-3xl text-red-700" />
                         </div>
-                        <span className="text-lg">{EMAIL1}</span>
+                        <span className="text-lg">{CONTACT_INFO.EMAIL_1}</span>
                     </li>
                     <li className="flex items-center gap-3">
                         <div className="rounded-lg bg-white p-1">
                             <img src={EmailIcon} className="text-3xl text-red-700" />
                         </div>
-                        <span className="text-lg">{EMAIL2}</span>
+                        <span className="text-lg">{CONTACT_INFO.EMAIL_2}</span>
                     </li>
                 </ul>
             </div>
@@ -47,19 +39,19 @@ export function ContactSection() {
                         <div className="rounded-lg bg-white p-1">
                             <img src={FacebookIcon} className="text-3xl text-red-700" />
                         </div>
-                        <span className="text-lg">{FACEBOOK}</span>
+                        <span className="text-lg">{CONTACT_INFO.FACEBOOK}</span>
                     </li>
                     <li className="flex items-center gap-3">
                         <div className="rounded-lg bg-white p-1">
                             <img src={InstagramIcon} className="text-3xl text-red-700" />
                         </div>
-                        <span className="text-lg">{INSTAGRAM}</span>
+                        <span className="text-lg">{CONTACT_INFO.INSTAGRAM}</span>
                     </li>
                     <li className="flex items-center gap-3">
                         <div className="rounded-lg bg-white p-1">
                             <img src={LinkedinIcon} className="text-3xl text-red-700" />
                         </div>
-                        <span className="text-lg font-semibold underline">{LINKEDIN}</span>
+                        <span className="text-lg font-semibold underline">{CONTACT_INFO.LINKEDIN}</span>
                     </li>
                 </ul>
             </div>
