8648a0312ecc1f91fe5a21556a137e8eb748ba62
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-11 17:02:13 -0300

    Add links in ContactSection.jsx

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,5 +1,6 @@
 import { EmailIcon, FacebookIcon, InstagramIcon, LinkedinIcon, WhatsappIcon } from "../assets/icons"
 import { CONTACT_INFO } from "../utils/contactInfo"
+import { Link } from "react-router"
 
 export function ContactSection() {
     /* Seccion de contacto */
@@ -9,51 +10,51 @@ export function ContactSection() {
             {/* Contacto */}
             <div className="text-center md:text-left">
                 <h2 className="mb-4 text-3xl font-bold">CONTÁCTANOS</h2>
-                <ul className="space-y-3">
-                    <li className="flex items-center gap-3">
+                <nav className="space-y-3">
+                    <Link to={CONTACT_INFO.PHONE_LINK} target className="flex items-center gap-3">
                         <div className="rounded-lg bg-white p-1">
                             <img src={WhatsappIcon} className="text-3xl text-red-700" />
                         </div>
                         <span className="text-lg font-semibold">{CONTACT_INFO.PHONE}</span>
-                    </li>
-                    <li className="flex items-center gap-3">
+                    </Link>
+                    <Link to={CONTACT_INFO.EMAIL_1_LINK} className="flex items-center gap-3">
                         <div className="rounded-lg bg-white p-1">
                             <img src={EmailIcon} className="text-3xl text-red-700" />
                         </div>
                         <span className="text-lg">{CONTACT_INFO.EMAIL_1}</span>
-                    </li>
-                    <li className="flex items-center gap-3">
+                    </Link>
+                    <Link to={CONTACT_INFO.EMAIL_2_LINK} className="flex items-center gap-3">
                         <div className="rounded-lg bg-white p-1">
                             <img src={EmailIcon} className="text-3xl text-red-700" />
                         </div>
                         <span className="text-lg">{CONTACT_INFO.EMAIL_2}</span>
-                    </li>
-                </ul>
+                    </Link>
+                </nav>
             </div>
 
             {/* Redes Sociales */}
             <div className="text-center md:text-left">
                 <h2 className="mb-4 text-3xl font-bold">SÍGUENOS EN REDES</h2>
-                <ul className="space-y-3">
-                    <li className="flex items-center gap-3">
+                <nav className="space-y-3">
+                    <Link to={CONTACT_INFO.FACEBOOK_LINK} className="flex items-center gap-3">
                         <div className="rounded-lg bg-white p-1">
                             <img src={FacebookIcon} className="text-3xl text-red-700" />
                         </div>
                         <span className="text-lg">{CONTACT_INFO.FACEBOOK}</span>
-                    </li>
-                    <li className="flex items-center gap-3">
+                    </Link>
+                    <Link to={CONTACT_INFO.INSTAGRAM_LINK} className="flex items-center gap-3">
                         <div className="rounded-lg bg-white p-1">
                             <img src={InstagramIcon} className="text-3xl text-red-700" />
                         </div>
                         <span className="text-lg">{CONTACT_INFO.INSTAGRAM}</span>
-                    </li>
-                    <li className="flex items-center gap-3">
+                    </Link>
+                    <Link to={CONTACT_INFO.LINKEDIN_LINK} className="flex items-center gap-3">
                         <div className="rounded-lg bg-white p-1">
                             <img src={LinkedinIcon} className="text-3xl text-red-700" />
                         </div>
                         <span className="text-lg font-semibold underline">{CONTACT_INFO.LINKEDIN}</span>
-                    </li>
-                </ul>
+                    </Link>
+                </nav>
             </div>
         </div>
     )
