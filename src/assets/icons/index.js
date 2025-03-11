cdd797a5d5cd565682dc83dd55fe74135db07d32
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-11 13:02:59 -0300

    Add index.js to centralize icon imports

---
+++ src/assets/icons/index.js
@@ -0,0 +1,23 @@
+import EmailIcon from "./ICONO-Email.svg"
+import EmailTransparentIcon from "./ICONO-Email-transparente.svg"
+import FacebookIcon from "./ICONO-Facebook.svg"
+import FacebookTransparentIcon from "./ICONO-Facebook-transparente.svg"
+import InstagramIcon from "./ICONO-Instagram.svg"
+import InstagramTransparentIcon from "./ICONO-Instagram-transparente.svg"
+import LinkedinIcon from "./ICONO-LinkedIn.svg"
+import LinkedinTransparentIcon from "./ICONO-Linkedin-transparente.svg"
+import WhatsappIcon from "./ICONO-Whatsapp.svg"
+import WhatsappTransparentIcon from "./ICONO-Whatsapp-transparente.svg"
+
+export {
+    EmailIcon,
+    EmailTransparentIcon,
+    FacebookIcon,
+    FacebookTransparentIcon,
+    InstagramIcon,
+    InstagramTransparentIcon,
+    LinkedinIcon,
+    LinkedinTransparentIcon,
+    WhatsappIcon,
+    WhatsappTransparentIcon,
+}
