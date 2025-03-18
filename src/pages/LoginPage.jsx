ddcda2fd70c60fc64258cfcae6dd5c8eb043a712
MGIC12 <mager.infante@gmail.com>
2025-03-18 16:46:26 -0300

    Estructura Login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,19 @@
+import { ColorBar } from "../components"
 export function LoginPage() {
-    return <div>LoginPage</div>
+    return (
+        <div className="flex h-screen grow flex-col">
+            <header className="h-1/5">
+                <ColorBar />
+                {/* Reemplazar etiqueta nav por componente Navbar */}
+                <nav className="flex items-center justify-between p-7">
+                    <h1 className="text-3xl font-bold">Logo FARA</h1>
+                    <h2 className="font-bold text-red-500">Aca va el componente Navbar</h2>
+                    <span className="text-lg font-semibold">Links</span>
+                </nav>
+            </header>
+            <footer className="flex-col gap-4">
+                <ColorBar />
+            </footer>
+        </div>
+    )
 }
