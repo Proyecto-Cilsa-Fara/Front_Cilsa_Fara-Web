6da89ed31f8efa57def8e2b63d28db942d49a7b8
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-17 21:15:12 -0300

    Fix mobile layout breaking due to font-sizing

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -13,10 +13,10 @@ export function ContactSection() {
     /* Seccion de contacto */
 
     return (
-        <div className="flex w-full flex-col justify-center gap-18 bg-red-700 p-20 text-white md:flex-row">
+        <div className="flex w-full flex-col justify-center gap-9 bg-red-700 p-10 text-white lg:flex-row lg:gap-18 lg:p-20">
             {/* Contacto */}
-            <div className="text-center md:text-left">
-                <h2 className="mb-4 text-3xl font-bold">CONTÁCTANOS</h2>
+            <div className="text-center lg:text-left">
+                <h2 className="mb-4 text-xl font-bold lg:text-3xl">CONTÁCTANOS</h2>
                 <nav className="space-y-3" aria-label="Opciones de contacto">
                     <Link
                         to={CONTACT_INFO.PHONE_LINK}
@@ -25,7 +25,7 @@ export function ContactSection() {
                         aria-label="Envíanos un mensaje por Whatsapp"
                     >
                         <img src={WhatsappTransparentIcon} className="w-10" alt="Whatsapp" />
-                        <span className="text-lg font-semibold">{CONTACT_INFO.PHONE}</span>
+                        <span className="text-sm font-semibold lg:text-lg">{CONTACT_INFO.PHONE}</span>
                     </Link>
                     <Link
                         to={CONTACT_INFO.EMAIL_1_LINK}
@@ -33,7 +33,7 @@ export function ContactSection() {
                         aria-label="Envíanos un correo electrónico"
                     >
                         <img src={EmailTransparentIcon} className="w-10" alt="Correo electrónico" />
-                        <span className="text-lg font-semibold">{CONTACT_INFO.EMAIL_1}</span>
+                        <span className="text-sm font-semibold lg:text-lg">{CONTACT_INFO.EMAIL_1}</span>
                     </Link>
                     <Link
                         to={CONTACT_INFO.EMAIL_2_LINK}
@@ -41,14 +41,14 @@ export function ContactSection() {
                         aria-label="Envíanos un correo electrónico"
                     >
                         <img src={EmailTransparentIcon} className="w-10" alt="Correo electrónico" />
-                        <span className="text-lg font-semibold">{CONTACT_INFO.EMAIL_2}</span>
+                        <span className="text-sm font-semibold lg:text-lg">{CONTACT_INFO.EMAIL_2}</span>
                     </Link>
                 </nav>
             </div>
 
             {/* Redes Sociales */}
-            <div className="text-center md:text-left">
-                <h2 className="mb-4 text-3xl font-bold">SÍGUENOS EN REDES</h2>
+            <div className="text-center lg:text-left">
+                <h2 className="mb-4 text-xl font-bold lg:text-3xl">SÍGUENOS EN REDES</h2>
                 <nav className="space-y-3" aria-label="Nuestras redes sociales">
                     <Link
                         to={CONTACT_INFO.FACEBOOK_LINK}
@@ -56,7 +56,7 @@ export function ContactSection() {
                         aria-label="Síguenos en Facebook"
                     >
                         <img src={FacebookTransparentIcon} className="w-10" alt="Facebook" />
-                        <span className="text-lg font-semibold">{CONTACT_INFO.FACEBOOK}</span>
+                        <span className="text-sm font-semibold lg:text-lg">{CONTACT_INFO.FACEBOOK}</span>
                     </Link>
                     <Link
                         to={CONTACT_INFO.INSTAGRAM_LINK}
@@ -64,7 +64,7 @@ export function ContactSection() {
                         aria-label="Síguenos en Instagram"
                     >
                         <img src={InstagramTransparentIcon} className="w-10" alt="Instagram" />
-                        <span className="text-lg font-semibold">{CONTACT_INFO.INSTAGRAM}</span>
+                        <span className="text-sm font-semibold lg:text-lg">{CONTACT_INFO.INSTAGRAM}</span>
                     </Link>
                     <Link
                         to={CONTACT_INFO.LINKEDIN_LINK}
@@ -72,7 +72,7 @@ export function ContactSection() {
                         aria-label="Síguenos en Linkedin"
                     >
                         <img src={LinkedinTransparentIcon} className="w-10" alt="Linkedin" />
-                        <span className="text-lg font-semibold">{CONTACT_INFO.LINKEDIN}</span>
+                        <span className="text-sm font-semibold lg:text-lg">{CONTACT_INFO.LINKEDIN}</span>
                     </Link>
                     <Link
                         to={CONTACT_INFO.YOUTUBE_LINK}
@@ -80,7 +80,7 @@ export function ContactSection() {
                         aria-label="Síguenos en Youtube"
                     >
                         <img src={YoutubeTransparentIcon} className="w-10" alt="Youtube" />
-                        <span className="text-lg font-semibold">{CONTACT_INFO.YOUTUBE}</span>
+                        <span className="text-sm font-semibold lg:text-lg">{CONTACT_INFO.YOUTUBE}</span>
                     </Link>
                 </nav>
             </div>
