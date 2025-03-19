eed60ff7d14598882c8aa58d7904bc9689e7dbb4
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-19 03:28:03 -0300

    Add hero-carrousel images and index.js

---
+++ src/assets/hero-carrousel/index.js
@@ -0,0 +1,7 @@
+import img1 from "./Hero1.webp"
+import img2 from "./Hero2.webp"
+import img3 from "./Hero3.webp"
+import img4 from "./Hero4.webp"
+import img5 from "./Hero5.webp"
+
+export { img1, img2, img3, img4, img5 }
