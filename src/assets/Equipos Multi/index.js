398512f4f9a9460586f28300cdb562ddb8e16232
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-19 02:25:07 -0300

    Add index.js in Equipos-multidisciplinarios for export centralization

---
+++ src/assets/Equipos Multi/index.js
@@ -0,0 +1,10 @@
+import imgCircArt from "../assets/Equipos Multi/CirculosDeArte.jpg"
+import Educacion from "../assets/Equipos Multi/Educacion.jpg"
+import Oficios from "../assets/Equipos Multi/Escuela de oficios.jpg"
+import Facilitacion from "../assets/Equipos Multi/Facilitacion y mediacion.jpg"
+import Justicia from "../assets/Equipos Multi/Justicia para victimas.jpg"
+import Niñez from "../assets/Equipos Multi/Niñez y juventud.jpg"
+import Politicas from "../assets/Equipos Multi/Politicas Publicas.jpg"
+import Violencia from "../assets/Equipos Multi/Violencia.jpg"
+
+export { imgCircArt, Educacion, Oficios, Facilitacion, Justicia, Niñez, Politicas, Violencia }
