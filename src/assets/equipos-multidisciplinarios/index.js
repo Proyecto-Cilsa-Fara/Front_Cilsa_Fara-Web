71c60562e777833105af35f26f514b7666067063
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-19 02:28:02 -0300

    Fix import paths in the index.js of equipos-multidisciplinarios

--- src/assets/equipos-multidisciplinarios/index.js
+++ src/assets/equipos-multidisciplinarios/index.js
@@ -1,10 +1,10 @@
-import imgCircArt from "../assets/Equipos Multi/CirculosDeArte.jpg"
-import Educacion from "../assets/Equipos Multi/Educacion.jpg"
-import Oficios from "../assets/Equipos Multi/Escuela de oficios.jpg"
-import Facilitacion from "../assets/Equipos Multi/Facilitacion y mediacion.jpg"
-import Justicia from "../assets/Equipos Multi/Justicia para victimas.jpg"
-import Niñez from "../assets/Equipos Multi/Niñez y juventud.jpg"
-import Politicas from "../assets/Equipos Multi/Politicas Publicas.jpg"
-import Violencia from "../assets/Equipos Multi/Violencia.jpg"
+import imgCircArt from "./CirculosDeArte.jpg"
+import Educacion from "./Educacion.jpg"
+import Oficios from "./Escuela de oficios.jpg"
+import Facilitacion from "./Facilitacion y mediacion.jpg"
+import Justicia from "./Justicia para victimas.jpg"
+import Niñez from "./Niñez y juventud.jpg"
+import Politicas from "./Politicas Publicas.jpg"
+import Violencia from "./Violencia.jpg"
 
 export { imgCircArt, Educacion, Oficios, Facilitacion, Justicia, Niñez, Politicas, Violencia }
