c26c0b7f55a4c51393ab186136a95108088f8dd9
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-19 02:35:56 -0300

    Optimize asset naming in equipos-multidisciplinarios images

--- src/assets/equipos-multidisciplinarios/index.js
+++ src/assets/equipos-multidisciplinarios/index.js
@@ -1,10 +1,10 @@
-import imgCircArt from "./CirculosDeArte.jpg"
+import imgCircArt from "./Circulos-de-arte.jpg"
 import Educacion from "./Educacion.jpg"
-import Oficios from "./Escuela de oficios.jpg"
-import Facilitacion from "./Facilitacion y mediacion.jpg"
-import Justicia from "./Justicia para victimas.jpg"
-import Niñez from "./Niñez y juventud.jpg"
-import Politicas from "./Politicas Publicas.jpg"
+import Oficios from "./Escuela-de-oficios.jpg"
+import Facilitacion from "./Facilitacion-y-mediacion.jpg"
+import Justicia from "./Justicia-para-victimas.jpg"
+import Niñez from "./Niñez-y-juventud.jpg"
+import Politicas from "./Politicas-Publicas.jpg"
 import Violencia from "./Violencia.jpg"
 
 export { imgCircArt, Educacion, Oficios, Facilitacion, Justicia, Niñez, Politicas, Violencia }
