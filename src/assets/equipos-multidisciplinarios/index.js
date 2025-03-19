4efe35a228fe714b7b2cd22252d9f89f8d89b10d
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-19 02:40:39 -0300

    Reformat jpg images in equipos-multidisciplinarios to webp

--- src/assets/equipos-multidisciplinarios/index.js
+++ src/assets/equipos-multidisciplinarios/index.js
@@ -1,10 +1,10 @@
-import imgCircArt from "./Circulos-de-arte.jpg"
-import Educacion from "./Educacion.jpg"
-import Oficios from "./Escuela-de-oficios.jpg"
-import Facilitacion from "./Facilitacion-y-mediacion.jpg"
-import Justicia from "./Justicia-para-victimas.jpg"
-import Niñez from "./Niñez-y-juventud.jpg"
-import Politicas from "./Politicas-Publicas.jpg"
-import Violencia from "./Violencia.jpg"
+import imgCircArt from "./Circulos-de-arte.webp"
+import Educacion from "./Educacion.webp"
+import Oficios from "./Escuela-de-oficios.webp"
+import Facilitacion from "./Facilitacion-y-mediacion.webp"
+import Justicia from "./Justicia-para-victimas.webp"
+import Niñez from "./Niñez-y-juventud.webp"
+import Politicas from "./Politicas-Publicas.webp"
+import Violencia from "./Violencia.webp"
 
 export { imgCircArt, Educacion, Oficios, Facilitacion, Justicia, Niñez, Politicas, Violencia }
