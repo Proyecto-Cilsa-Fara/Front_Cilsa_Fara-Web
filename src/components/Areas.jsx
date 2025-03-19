8ed9872d762e94af2aca8a3fd06f95d2bf31adb3
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-19 02:28:26 -0300

    Add improved asset imports in Areas.jsx

--- src/components/Areas.jsx
+++ src/components/Areas.jsx
@@ -1,11 +1,13 @@
-import imgCircArt from "../assets/Equipos Multi/CirculosDeArte.jpg"
-import Educacion from "../assets/Equipos Multi/Educacion.jpg"
-import Oficios from "../assets/Equipos Multi/Escuela de oficios.jpg"
-import Facilitacion from "../assets/Equipos Multi/Facilitacion y mediacion.jpg"
-import Justicia from "../assets/Equipos Multi/Justicia para victimas.jpg"
-import Niñez from "../assets/Equipos Multi/Niñez y juventud.jpg"
-import Politicas from "../assets/Equipos Multi/Politicas Publicas.jpg"
-import Violencia from "../assets/Equipos Multi/Violencia.jpg"
+import {
+    imgCircArt,
+    Educacion,
+    Oficios,
+    Facilitacion,
+    Justicia,
+    Niñez,
+    Politicas,
+    Violencia,
+} from "../assets/equipos-multidisciplinarios"
 
 export function Areas() {
     const areas = [
