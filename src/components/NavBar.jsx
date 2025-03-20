ccfac9e6bdb6bfc2294d7d8a19bcdeb64286d30d
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-20 03:03:24 -0300

    Fix button accesibility in NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
                 <div className="flex items-center space-x-4">
                     {/* Botón de menú para móviles */}
                     <button className="md:hidden" onClick={() => setIsOpen(!isOpen)} aria-label="Toggle Menu">
-                        {isOpen ? <img src={X} className="h-8 w-8" /> : <img src={MenuIcon} className="h-8 w-8" />}
+                        {isOpen ? <img src={X} className="h-12 w-12" /> : <img src={MenuIcon} className="h-12 w-12" />}
                     </button>
 
                     <Link to="#">
@@ -22,17 +22,17 @@ export default function Navbar() {
                 {/* Menú principal */}
                 <ul className="mr-12 ml-auto hidden space-x-6 font-bold uppercase md:flex">
                     <li>
-                        <Link to="#" className="hover:underline">
+                        <Link to="#" className="grid min-h-12 min-w-12 place-items-center hover:underline">
                             Institucional
                         </Link>
                     </li>
                     <li>
-                        <Link to="#" className="hover:underline">
+                        <Link to="#" className="grid min-h-12 min-w-12 place-items-center hover:underline">
                             Capacitaciones
                         </Link>
                     </li>
                     <li>
-                        <Link to="#" className="hover:underline">
+                        <Link to="#" className="grid min-h-12 min-w-12 place-items-center hover:underline">
                             Socios
                         </Link>
                     </li>
@@ -40,10 +40,10 @@ export default function Navbar() {
 
                 {/* Botones en la barra de navegación */}
                 <div className="flex space-x-4">
-                    <button className="skew-custom bg-blue-500 px-4 py-2 text-white uppercase hover:bg-blue-600">
+                    <button className="skew-custom min-h-12 min-w-12 bg-blue-500 py-2 pr-8 pl-6 text-white uppercase hover:bg-blue-600">
                         <p className="text-xl font-bold drop-shadow-[0.7px_0.7px_0.7px_black]">Doná</p>
                     </button>
-                    <button className="skew-custom bg-red-700 text-white hover:bg-red-800">
+                    <button className="skew-custom min-h-12 min-w-12 bg-red-700 py-2 pr-8 pl-6 text-white hover:bg-red-800">
                         <p className="text-xl font-bold drop-shadow-[0.7px_0.7px_0.7px_black]">Participá</p>
                     </button>
                 </div>
@@ -51,19 +51,19 @@ export default function Navbar() {
 
             {/* Menú desplegable en móviles */}
             {isOpen && (
-                <ul className="mt-4 flex flex-col space-y-2 p-4 font-bold uppercase md:hidden">
+                <ul className="mt-4 flex flex-col gap-2 font-bold uppercase md:hidden">
                     <li>
-                        <Link to="#" className="block py-2">
+                        <Link to="#" className="grid min-h-12 min-w-12 place-items-center">
                             Institucional
                         </Link>
                     </li>
                     <li>
-                        <Link to="#" className="block py-2">
+                        <Link to="#" className="grid min-h-12 min-w-12 place-items-center">
                             Capacitaciones
                         </Link>
                     </li>
                     <li>
-                        <Link to="#" className="block py-2">
+                        <Link to="#" className="grid min-h-12 min-w-12 place-items-center">
                             Socios
                         </Link>
                     </li>
