3bcb0d255d45b08a6a0a0c4fee8f440d0b97fda6
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-20 18:13:58 -0300

    Add text accesibility improvements in ServiciosSections.jsx

--- src/components/ServiciosSection.jsx
+++ src/components/ServiciosSection.jsx
@@ -7,10 +7,8 @@ export function ServiciosSection() {
             <div className="grid gap-8 text-center md:grid-cols-3">
                 <div className="flex h-full flex-col items-center gap-5">
                     <img src={BoneteIcon} className="h-37 p-10 lg:h-48" alt="Icono gorro de graduacion" />
-                    <h3 className="text-2xl font-bold drop-shadow-[0.7px_0.7px_0.7px_black] lg:text-3xl">
-                        CAPACITACIONES
-                    </h3>
-                    <p className="text-md flex-grow font-bold drop-shadow-[0.7px_0.7px_0.7px_black]">
+                    <h3 className="text-outline-sm text-2xl font-bold lg:text-3xl">CAPACITACIONES</h3>
+                    <p className="text-md text-outline-sm flex-grow font-bold">
                         Capacitamos a profesionales y empleados estatales en resolución de conflictos y trabajo en
                         contextos de encierro.
                     </p>
@@ -19,15 +17,15 @@ export function ServiciosSection() {
                         className="group skew-custom bg-white p-1 hover:cursor-pointer"
                         aria-label="Ver más sobre capacitaciones"
                     >
-                        <div className="skew-custom text-md bg-fara-teal px-6 py-1.5 font-bold drop-shadow-[0.7px_0.7px_0.7px_black] transition-colors duration-300 ease-in-out group-hover:bg-black group-focus:bg-black">
-                            <span className="drop-shadow-[0.7px_0.7px_0.7px_black]">VER MÁS</span>
+                        <div className="skew-custom text-md bg-fara-teal text-outline-sm px-6 py-1.5 font-bold transition-colors duration-300 ease-in-out group-hover:bg-black group-focus:bg-black">
+                            <span className="text-outline-sm">VER MÁS</span>
                         </div>
                     </Link>
                 </div>
                 <div className="flex h-full flex-col items-center gap-5">
                     <img src={TuercaIcon} className="h-37 p-10 lg:h-48" alt="Icono servicios" />
-                    <h3 className="text-2xl font-bold drop-shadow-[0.7px_0.7px_0.7px_black] lg:text-3xl">SERVICIOS</h3>
-                    <p className="text-md flex-grow font-bold drop-shadow-[0.7px_0.7px_0.7px_black]">
+                    <h3 className="text-outline-sm text-2xl font-bold lg:text-3xl">SERVICIOS</h3>
+                    <p className="text-md text-outline-sm flex-grow font-bold">
                         Asesoramos a organismos y organizaciones en la gestión de conflictos con un enfoque integral y
                         restaurativo.
                     </p>
@@ -36,19 +34,15 @@ export function ServiciosSection() {
                         className="group skew-custom bg-white p-1 hover:cursor-pointer"
                         aria-label="Consultar sobre servicios"
                     >
-                        <div className="skew-custom text-md bg-fara-teal px-6 py-1.5 font-bold drop-shadow-[0.7px_0.7px_0.7px_black] transition-colors duration-300 ease-in-out group-hover:bg-black group-focus:bg-black">
-                            <span className="drop-shadow-[0.7px_0.7px_0.7px_black] group-hover:text-white">
-                                CONSULTAR
-                            </span>
+                        <div className="skew-custom text-md bg-fara-teal text-outline-sm px-6 py-1.5 font-bold transition-colors duration-300 ease-in-out group-hover:bg-black group-focus:bg-black">
+                            <span className="text-outline-sm group-hover:text-white">CONSULTAR</span>
                         </div>
                     </Link>
                 </div>
                 <div className="flex h-full flex-col items-center gap-5">
                     <img src={ManoIcon} className="h-37 p-10 lg:h-48" alt="Icono mano donar" />
-                    <h3 className="text-2xl font-bold drop-shadow-[0.7px_0.7px_0.7px_black] lg:text-3xl">
-                        VOLUNTARIADO
-                    </h3>
-                    <p className="text-md flex-grow font-bold drop-shadow-[0.7px_0.7px_0.7px_black]">
+                    <h3 className="text-outline-sm text-2xl font-bold lg:text-3xl">VOLUNTARIADO</h3>
+                    <p className="text-md text-outline-sm flex-grow font-bold">
                         Brindamos pasantías y voluntariados en las áreas de acción restaurativa de Fara.
                     </p>
                     <Link
@@ -56,8 +50,8 @@ export function ServiciosSection() {
                         className="group skew-custom bg-white p-1 hover:cursor-pointer"
                         aria-label="Participar en voluntariado"
                     >
-                        <div className="skew-custom text-md bg-fara-teal px-6 py-1.5 font-bold drop-shadow-[0.7px_0.7px_0.7px_black] transition-colors duration-300 ease-in-out group-hover:bg-black group-focus:bg-black">
-                            <span className="drop-shadow-[0.7px_0.7px_0.7px_black]">PARTICIPA</span>
+                        <div className="skew-custom text-md bg-fara-teal text-outline-sm px-6 py-1.5 font-bold transition-colors duration-300 ease-in-out group-hover:bg-black group-focus:bg-black">
+                            <span className="text-outline-sm">PARTICIPA</span>
                         </div>
                     </Link>
                 </div>
