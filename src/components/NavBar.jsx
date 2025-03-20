078674e04bb9ffd1d2cc85ae225efa8def5b0995
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-20 03:20:40 -0300

    Refactor button layout in NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,45 +7,45 @@ export default function Navbar() {
 
     return (
         <nav className="bg-white p-4 text-black">
-            <div className="container mx-auto flex items-center justify-between">
-                <div className="flex items-center space-x-4">
+            <div className="container flex items-center justify-between">
+                <Link to="#">
+                    <img src="/web-app-manifest-512x512.png" alt="Logo" className="w-20" />
+                </Link>
+
+                <div className="flex gap-8">
                     {/* Botón de menú para móviles */}
                     <button className="md:hidden" onClick={() => setIsOpen(!isOpen)} aria-label="Toggle Menu">
                         {isOpen ? <img src={X} className="h-12 w-12" /> : <img src={MenuIcon} className="h-12 w-12" />}
                     </button>
 
-                    <Link to="#">
-                        <img src="/web-app-manifest-512x512.png" alt="Logo" className="w-20" />
-                    </Link>
-                </div>
+                    {/* Menú principal */}
+                    <ul className="hidden gap-4 font-bold uppercase md:flex">
+                        <li>
+                            <Link to="#" className="grid min-h-12 min-w-12 place-items-center hover:underline">
+                                Institucional
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="grid min-h-12 min-w-12 place-items-center hover:underline">
+                                Capacitaciones
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="#" className="grid min-h-12 min-w-12 place-items-center hover:underline">
+                                Socios
+                            </Link>
+                        </li>
+                    </ul>
 
-                {/* Menú principal */}
-                <ul className="mr-12 ml-auto hidden space-x-6 font-bold uppercase md:flex">
-                    <li>
-                        <Link to="#" className="grid min-h-12 min-w-12 place-items-center hover:underline">
-                            Institucional
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#" className="grid min-h-12 min-w-12 place-items-center hover:underline">
-                            Capacitaciones
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#" className="grid min-h-12 min-w-12 place-items-center hover:underline">
-                            Socios
-                        </Link>
-                    </li>
-                </ul>
-
-                {/* Botones en la barra de navegación */}
-                <div className="flex space-x-4">
-                    <button className="skew-custom min-h-12 min-w-12 bg-blue-500 py-2 pr-8 pl-6 text-white uppercase hover:bg-blue-600">
-                        <p className="text-xl font-bold drop-shadow-[0.7px_0.7px_0.7px_black]">Doná</p>
-                    </button>
-                    <button className="skew-custom min-h-12 min-w-12 bg-red-700 py-2 pr-8 pl-6 text-white hover:bg-red-800">
-                        <p className="text-xl font-bold drop-shadow-[0.7px_0.7px_0.7px_black]">Participá</p>
-                    </button>
+                    {/* Botones en la barra de navegación */}
+                    <div className="hidden gap-4 md:flex">
+                        <button className="skew-custom min-h-12 min-w-48 bg-blue-500 py-2 pr-8 pl-6 text-white uppercase hover:bg-blue-600">
+                            <p className="text-xl font-bold drop-shadow-[0.7px_0.7px_0.7px_black]">Doná</p>
+                        </button>
+                        <button className="skew-custom min-h-12 min-w-48 bg-red-700 py-2 pr-8 pl-6 text-white hover:bg-red-800">
+                            <p className="text-xl font-bold drop-shadow-[0.7px_0.7px_0.7px_black]">Participá</p>
+                        </button>
+                    </div>
                 </div>
             </div>
 
@@ -66,6 +66,16 @@ export default function Navbar() {
                         <Link to="#" className="grid min-h-12 min-w-12 place-items-center">
                             Socios
                         </Link>
+                    </li>
+                    <li className="self-center">
+                        <button className="skew-custom min-h-12 min-w-48 bg-blue-500 pr-8 pl-6 text-white uppercase hover:bg-blue-600">
+                            <p className="text-xl font-bold drop-shadow-[0.7px_0.7px_0.7px_black]">Doná</p>
+                        </button>
+                    </li>
+                    <li className="self-center">
+                        <button className="skew-custom min-h-12 min-w-48 bg-red-700 pr-8 pl-6 text-white hover:bg-red-800">
+                            <p className="text-xl font-bold drop-shadow-[0.7px_0.7px_0.7px_black]">Participá</p>
+                        </button>
                     </li>
                 </ul>
             )}
