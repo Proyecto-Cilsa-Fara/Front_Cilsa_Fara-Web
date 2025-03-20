83150fc87251a863011a73a6598f92775c036585
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-20 17:41:29 -0300

    Adjust background color in AcercaDe.jsx

--- src/components/AcercaDe.jsx
+++ src/components/AcercaDe.jsx
@@ -1,6 +1,6 @@
 export function AcercaDe() {
     return (
-        <div className="w-full bg-blue-500 py-24 text-center text-white max-sm:p-10 md:px-15">
+        <div className="bg-fara-cyan w-full py-24 text-center text-white max-sm:p-10 md:px-15">
             <h2 className="text-outline-lg m-auto px-5 text-3xl font-bold max-md:text-2xl">
                 Asesoramos, acompañamos y desarrollamos programas mediante el paradigma restaurativo, entrenando y
                 capacitando operadores para su implementación.
