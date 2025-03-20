ad77c42bbbfd0998c6ed7cec6ebd3c37da5de524
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-20 03:27:06 -0300

    Add cursor-pointer on hover for buttons in NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
     return (
         <nav className="bg-white p-4 text-black">
             <div className="container flex items-center justify-between">
-                <Link to="#">
+                <Link to="#" className="hover:cursor-pointer">
                     <img
                         src={faraLogoImage}
                         alt="Logo de FARA que actúa como link a la página de inicio."
@@ -26,17 +26,26 @@ export default function Navbar() {
                     {/* Menú principal */}
                     <ul className="hidden gap-4 font-bold uppercase md:flex">
                         <li>
-                            <Link to="#" className="grid min-h-12 min-w-12 place-items-center hover:underline">
+                            <Link
+                                to="#"
+                                className="grid min-h-12 min-w-12 place-items-center hover:cursor-pointer hover:underline"
+                            >
                                 Institucional
                             </Link>
                         </li>
                         <li>
-                            <Link to="#" className="grid min-h-12 min-w-12 place-items-center hover:underline">
+                            <Link
+                                to="#"
+                                className="grid min-h-12 min-w-12 place-items-center hover:cursor-pointer hover:underline"
+                            >
                                 Capacitaciones
                             </Link>
                         </li>
                         <li>
-                            <Link to="#" className="grid min-h-12 min-w-12 place-items-center hover:underline">
+                            <Link
+                                to="#"
+                                className="grid min-h-12 min-w-12 place-items-center hover:cursor-pointer hover:underline"
+                            >
                                 Socios
                             </Link>
                         </li>
@@ -44,10 +53,10 @@ export default function Navbar() {
 
                     {/* Botones en la barra de navegación */}
                     <div className="hidden gap-4 md:flex">
-                        <button className="skew-custom min-h-12 min-w-48 bg-blue-500 py-2 pr-8 pl-6 text-white uppercase hover:bg-blue-600">
+                        <button className="skew-custom min-h-12 min-w-48 bg-blue-500 py-2 pr-8 pl-6 text-white uppercase hover:cursor-pointer hover:bg-blue-600">
                             <p className="text-xl font-bold drop-shadow-[0.7px_0.7px_0.7px_black]">Doná</p>
                         </button>
-                        <button className="skew-custom min-h-12 min-w-48 bg-red-700 py-2 pr-8 pl-6 text-white hover:bg-red-800">
+                        <button className="skew-custom min-h-12 min-w-48 bg-red-700 py-2 pr-8 pl-6 text-white hover:cursor-pointer hover:bg-red-800">
                             <p className="text-xl font-bold drop-shadow-[0.7px_0.7px_0.7px_black]">Participá</p>
                         </button>
                     </div>
@@ -58,27 +67,27 @@ export default function Navbar() {
             {isOpen && (
                 <ul className="mt-4 flex flex-col gap-2 font-bold uppercase md:hidden">
                     <li>
-                        <Link to="#" className="grid min-h-12 min-w-12 place-items-center">
+                        <Link to="#" className="grid min-h-12 min-w-12 place-items-center hover:cursor-pointer">
                             Institucional
                         </Link>
                     </li>
                     <li>
-                        <Link to="#" className="grid min-h-12 min-w-12 place-items-center">
+                        <Link to="#" className="grid min-h-12 min-w-12 place-items-center hover:cursor-pointer">
                             Capacitaciones
                         </Link>
                     </li>
                     <li>
-                        <Link to="#" className="grid min-h-12 min-w-12 place-items-center">
+                        <Link to="#" className="grid min-h-12 min-w-12 place-items-center hover:cursor-pointer">
                             Socios
                         </Link>
                     </li>
                     <li className="self-center">
-                        <button className="skew-custom min-h-12 min-w-48 bg-blue-500 pr-8 pl-6 text-white uppercase hover:bg-blue-600">
+                        <button className="skew-custom min-h-12 min-w-48 bg-blue-500 pr-8 pl-6 text-white uppercase hover:cursor-pointer hover:bg-blue-600">
                             <p className="text-xl font-bold drop-shadow-[0.7px_0.7px_0.7px_black]">Doná</p>
                         </button>
                     </li>
                     <li className="self-center">
-                        <button className="skew-custom min-h-12 min-w-48 bg-red-700 pr-8 pl-6 text-white hover:bg-red-800">
+                        <button className="skew-custom min-h-12 min-w-48 bg-red-700 pr-8 pl-6 text-white hover:cursor-pointer hover:bg-red-800">
                             <p className="text-xl font-bold drop-shadow-[0.7px_0.7px_0.7px_black]">Participá</p>
                         </button>
                     </li>
