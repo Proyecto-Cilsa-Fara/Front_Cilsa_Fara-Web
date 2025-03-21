41acea864e6328ab37c0f9a6646d45e482ab1694
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-21 02:51:39 -0300

    Refactor AcercaDe.jsx to improve SEO and accesibility in LandingPage.jsx

--- src/components/AcercaDe.jsx
+++ src/components/AcercaDe.jsx
@@ -1,10 +1,13 @@
 export function AcercaDe() {
     return (
-        <div className="bg-fara-cyan w-full py-24 text-center text-white max-sm:p-10 md:px-15">
-            <h2 className="text-outline-lg m-auto px-5 text-3xl font-bold max-md:text-2xl">
-                Asesoramos, acompañamos y desarrollamos programas mediante el paradigma restaurativo, entrenando y
-                capacitando operadores para su implementación.
-            </h2>
-        </div>
+        <section className="bg-fara-cyan flex w-full flex-col gap-8 py-24 text-center text-white shadow-2xl max-sm:p-10 md:px-15 lg:gap-16">
+            <h1 className="text-outline-sm m-auto px-5 text-2xl font-extrabold md:text-4xl">
+                Transformamos vidas a través del paradigma restaurativo
+            </h1>
+            <p className="bg-fara-teal max-w-[65ch] self-center rounded-2xl border-2 border-white p-4 text-left text-pretty text-black shadow-2xs lg:text-3xl">
+                Impulsamos programas que promueven la justicia restaurativa, brindando asesoramiento, acompañamiento y
+                formación a operadores para que generen un impacto duradero en sus comunidades.
+            </p>
+        </section>
     )
 }
