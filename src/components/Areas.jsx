9b38893b5fed89178bfafc872ddb3d990535f70e
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-21 03:01:56 -0300

    Improve accesibility in Areas.jsx

--- src/components/Areas.jsx
+++ src/components/Areas.jsx
@@ -8,6 +8,7 @@ import {
     Politicas,
     Violencia,
 } from "../assets/equipos-multidisciplinarios"
+import { Link } from "react-router"
 
 export function Areas() {
     const areas = [
@@ -22,19 +23,21 @@ export function Areas() {
     ]
 
     return (
-        <div className="grid place-items-center px-4 py-16 text-pretty lg:px-32 lg:py-32">
+        <section className="flex flex-col justify-center px-4 py-16 text-pretty lg:px-32 lg:py-32">
             <h2 className="mb-6 text-center text-2xl font-bold md:text-3xl lg:text-5xl">
                 NUESTROS EQUIPOS MULTIDISCIPLINARIOS
             </h2>
-            <p className="md:text-2lg mb-12 text-pretty lg:text-3xl">
+            <p className="mb-12 max-w-[65ch] self-center text-pretty md:text-2xl lg:text-3xl">
                 La Fundación cuenta con equipos interdisciplinarios que estudian, analizan y desarrollan proyectos en
                 múltiples áreas de incidencia.
             </p>
             <div className="grid grid-cols-2 gap-4 md:grid-cols-3 xl:grid-cols-4">
                 {areas.map((area, index) => (
-                    <div
+                    <Link
                         key={index}
                         className="relative flex h-64 flex-col justify-end bg-cover bg-center p-4 text-center font-bold text-white drop-shadow-lg hover:cursor-pointer md:h-56 md:p-5 lg:h-96"
+                        to="/socios"
+                        aria-label={`Ver más acerca de ${area.name}.`}
                         style={{
                             backgroundImage: `url(${area.image})`,
                         }}
@@ -48,12 +51,12 @@ export function Areas() {
                         ></div>
 
                         {/* Texto alineado al fondo */}
-                        <span className="text-outline-sm relative z-10 mb-2 text-left text-sm text-white md:mb-3 md:text-xl lg:mb-4 lg:text-3xl">
+                        <h3 className="text-outline-sm relative z-10 mb-2 text-left text-sm text-white md:mb-3 md:text-xl lg:mb-4 lg:text-3xl">
                             {area.name}
-                        </span>
-                    </div>
+                        </h3>
+                    </Link>
                 ))}
             </div>
-        </div>
+        </section>
     )
 }
