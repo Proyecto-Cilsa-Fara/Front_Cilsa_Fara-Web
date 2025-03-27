03ee66f0c213927e048ba187d64c7ccca0aa72b1
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-27 03:23:27 -0300

    Add Typography improvements in AcercaDe.jsx

--- src/components/AcercaDe.jsx
+++ src/components/AcercaDe.jsx
@@ -1,11 +1,11 @@
 export function AcercaDe() {
     return (
-        <section className="bg-fara-cyan w-full py-24 text-center text-white max-sm:p-10 md:px-15">
-            <h1 className="text-outline-lg m-auto px-5 pb-4 text-5xl font-bold max-md:text-3xl">
+        <section className="bg-fara-cyan w-full py-24 text-center text-white max-sm:p-10 md:px-16">
+            <h1 className="text-outline-lg m-auto px-5 pb-4 text-5xl font-bold text-balance max-md:text-3xl">
                 FARA – La Fundación Acción Restaurativa Argentina
             </h1>
-            <hr className="mb-4 border-1 border-white" />
-            <p className="text-outline-lg m-auto max-w-[75ch] px-5 text-2xl font-bold max-md:text-left max-md:text-xl">
+            <hr className="my-4 border-1 border-white" />
+            <p className="text-outline-lg mx-auto max-w-[75ch] px-5 text-2xl font-bold text-balance max-md:text-xl">
                 Asesoramos, acompañamos y desarrollamos programas basados en el paradigma restaurativo, promoviendo la
                 resolución de conflictos y la reparación del daño. Capacitamos a operadores y profesionales para
                 implementar procesos restaurativos, fomentando el diálogo, la reconciliación y el fortalecimiento de la
