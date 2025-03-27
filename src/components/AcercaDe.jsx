1fd583d37e7ea17da850f9e643036b06673fb9c6
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-27 04:01:38 -0300

    Refactor design, typography and accesibility of AcercaDe.jsx

--- src/components/AcercaDe.jsx
+++ src/components/AcercaDe.jsx
@@ -1,16 +1,27 @@
+import FARA_BANNER_IMAGE from "../assets/BANNER-2-1024x267.webp"
+
 export function AcercaDe() {
     return (
-        <section className="bg-fara-cyan w-full py-24 text-center text-white max-sm:p-10 md:px-16">
-            <h1 className="text-outline-lg m-auto px-5 pb-4 text-5xl font-bold text-balance max-md:text-3xl">
-                FARA – La Fundación Acción Restaurativa Argentina
-            </h1>
-            <hr className="my-4 border-1 border-white" />
-            <p className="text-outline-lg mx-auto max-w-[75ch] px-5 text-2xl font-bold text-balance max-md:text-xl">
-                Asesoramos, acompañamos y desarrollamos programas basados en el paradigma restaurativo, promoviendo la
-                resolución de conflictos y la reparación del daño. Capacitamos a operadores y profesionales para
-                implementar procesos restaurativos, fomentando el diálogo, la reconciliación y el fortalecimiento de la
-                comunidad.
-            </p>
+        <section className="bg-fara-cyan w-full p-8 text-center text-black lg:p-24">
+            <div className="flex h-full flex-col items-center justify-evenly gap-4 bg-white p-8 shadow-2xl">
+                <img
+                    src={FARA_BANNER_IMAGE}
+                    alt="Logotipo de la Fundación Acción Restaurativa Argentina con bloques de color y texto"
+                    className="w-128 rounded-2xl shadow-md"
+                    height="267px"
+                    width="1024px"
+                />
+                <h1 className="text-2xl font-extrabold text-balance lg:text-4xl lg:font-bold">
+                    FARA – La Fundación Acción Restaurativa Argentina
+                </h1>
+                <hr className="my-1 w-4/5 border-1 border-black" />
+                <p className="text-xl text-balance lg:px-5">
+                    Asesoramos, acompañamos y desarrollamos programas basados en el paradigma restaurativo, promoviendo
+                    la resolución de conflictos y la reparación del daño. Capacitamos a operadores y profesionales para
+                    implementar procesos restaurativos, fomentando el diálogo, la reconciliación y el fortalecimiento de
+                    la comunidad.
+                </p>
+            </div>
         </section>
     )
 }
