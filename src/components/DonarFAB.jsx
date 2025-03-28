d3cd6e91605b8d1f33dfb20aa1f0c2feead6d90a
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-28 04:59:14 -0300

    Add primitive implementation of DonarFAB.jsx

--- src/components/DonarFAB.jsx
+++ src/components/DonarFAB.jsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router"
+
 export function DonarFAB() {
-    return <div>DonarFAB</div>
+    return (
+        <Link
+            to="/donar"
+            className="bg-fara-gold hover:bg-fara-gray active:bg-fara-gray focus:bg-fara-gray fixed right-4 bottom-16 z-10 grid h-28 w-28 place-items-center rounded-[50%] border-2 border-black text-xl font-bold text-black uppercase shadow-2xl transition-colors duration-300 ease-in-out hover:cursor-pointer hover:text-white focus:text-white active:text-white"
+        >
+            Donar
+        </Link>
+    )
 }
