5bb3573a052314febfb67f51bec84dd5c9074ae4
Matías Díaz <mati98ld@gmail.com>
2025-03-29 12:32:54 -0300

    Estructura general de pagina Institucional

---
+++ src/pages/InstitucionalPage.jsx
@@ -0,0 +1,52 @@
+import { ColorBar, ContactSection, DonarSection, Areas, NavBar, DonarFAB } from "../components/index"
+
+export function InstitucionalPage() {
+    const FOOTER_MESSAGE = "© 2025 Acción Restaurativa Argentina. Todos los derechos reservados."
+
+    return (
+        <div className="flex min-h-screen grow flex-col">
+            <header className="fixed z-10 flex w-full flex-col">
+                <ColorBar />
+
+                <NavBar />
+            </header>
+
+            <main className="mt-30 grid grow-[1] place-items-center">
+                {/* Socios Restaurativos (Reemplazar por componente SociosRestaurativos al div identificado)*/}
+                <div
+                    id="SociosRestaurativos"
+                    className="bg-fara-red w-full py-100 text-center text-3xl font-bold text-white"
+                >
+                    <h1>Acá va el componente SOCIOSRESTAURATIVOS</h1>
+                </div>
+
+                {/* Acerca De Nosotros (Reemplazar por componente AcercaDeNosotros al div identificado)*/}
+                <div id="AcercaDeNosotros" className="w-full py-100 text-center text-3xl font-bold">
+                    <h1>Acá va el componente ACERCA DE NOSOTROS</h1>
+                </div>
+
+                {/* Integrantes (Reemplazar por componente Integrantes al div identificado)*/}
+                <div id="Integrantes" className="bg-fara-teal w-full py-100 text-center text-3xl font-bold text-white">
+                    <h1>Acá va el componente INTEGRANTES</h1>
+                </div>
+
+                <DonarSection />
+
+                <Areas />
+
+                {/* Legales (Reemplazar por componente Legales al div identificado)*/}
+                <div id="Legales" className="bg-fara-gold w-full py-80 text-center text-3xl font-bold text-white">
+                    <h1>Acá va el componente LEGALES</h1>
+                </div>
+
+                <ContactSection />
+
+                <DonarFAB />
+            </main>
+            <footer className="w-full flex-col gap-4">
+                <p className="grid h-[calc(100%-1rem)] place-items-center p-4 text-sm">{FOOTER_MESSAGE}</p>
+                <ColorBar />
+            </footer>
+        </div>
+    )
+}
