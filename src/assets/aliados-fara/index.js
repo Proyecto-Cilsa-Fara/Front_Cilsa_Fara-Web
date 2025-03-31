2fc6929fb752cbd7a4e16f3fd1ad6bd785ba3d14
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-31 20:15:18 -0300

    Rename RACI import name in index.js

--- src/assets/aliados-fara/index.js
+++ src/assets/aliados-fara/index.js
@@ -4,7 +4,7 @@ import ComnapazMexicoImage from "./Comnapaz-Mexico.webp"
 import FundacionGuillermoSnopekImage from "./Fundacion-Guillermo-Snopek.webp"
 import InceipImage from "./INCEIP.webp"
 import MinisterioPublicoDeLaDefensaImage from "./Ministerio-Publico-de-la-defensa.webp"
-import RACI from "./RACI.webp"
+import RaciImage from "./RACI.webp"
 import RockefellerCenterImage from "./Rockefeller-Center.webp"
 import ServicioPenitenciarioBonaerenseImage from "./Servicio-Penitenciario-Bonaerense.webp"
 import SipinnaImage from "./SIPINNA.webp"
@@ -16,7 +16,7 @@ const aliadosImages = [
     FundacionGuillermoSnopekImage,
     InceipImage,
     MinisterioPublicoDeLaDefensaImage,
-    RACI,
+    RaciImage,
     RockefellerCenterImage,
     ServicioPenitenciarioBonaerenseImage,
     SipinnaImage,
