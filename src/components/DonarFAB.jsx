193be5f4c26ca1f1a931db52b0562ac850a101c0
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-03-31 20:12:18 -0300

    Add accessibility improvements in DonarFAB.jsx

--- src/components/DonarFAB.jsx
+++ src/components/DonarFAB.jsx
@@ -4,7 +4,7 @@ export function DonarFAB() {
     return (
         <Link
             to="/donar"
-            className="bg-fara-gold hover:bg-fara-gray active:bg-fara-gray focus:bg-fara-gray fixed right-4 bottom-16 z-10 grid h-28 w-28 place-items-center rounded-[50%] border-2 border-black text-xl font-bold text-black uppercase shadow-2xl transition-colors duration-300 ease-in-out hover:cursor-pointer hover:text-white focus:text-white active:text-white"
+            className="bg-fara-gold hover:bg-fara-gray active:bg-fara-gray focus:bg-fara-gray fixed right-4 bottom-16 z-10 grid h-16 w-16 place-items-center rounded-[50%] border-2 border-black text-xs font-bold text-black uppercase shadow-2xl transition-colors duration-300 ease-in-out hover:cursor-pointer hover:text-white focus:text-white active:text-white lg:h-28 lg:w-28 lg:border-4 lg:text-xl"
         >
             Donar
         </Link>
