5c101c6e2ce3b298466485385678d2645d412b89
Matías Díaz <mati98ld@gmail.com>
2025-04-01 23:51:22 -0300

    Create component 'Legales'

---
+++ src/components/Legales.jsx
@@ -0,0 +1,20 @@
+export function Legales() {
+    return (
+        <section className="bg-fara-gold relative w-full py-20">
+            <div className="skew-custom bg-fara-teal absolute -top-11 -z-5 h-12 w-1/2"></div>
+            <div className="bg-fara-cyan relative z-5 mx-auto w-60 -skew-x-20 py-4 sm:w-70 md:w-80">
+                <h1 className="skew-x-20 text-center text-4xl font-bold">LEGALES</h1>
+            </div>
+            <div className="mx-auto w-60 -translate-x-4 -translate-y-16 -skew-x-20 bg-black py-9 sm:w-70 md:w-80"></div>
+            <p className="lg:2xl px-4 text-center text-base text-balance md:px-20 md:text-xl xl:px-30">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laborum eveniet est enim eligendi
+                impedit in corrupti? Nam deleniti distinctio, eveniet molestiae assumenda facere, sapiente quaerat, amet
+                doloribus cupiditate officiis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+                laborum eveniet est enim eligendi impedit in corrupti? Nam deleniti distinctio, eveniet molestiae
+                assumenda facere, sapiente quaerat, amet doloribus cupiditate officiis! Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Repellendus laborum eveniet est enim eligendi
+            </p>
+            <div className="absolute bottom-0 h-8 w-full bg-black"></div>
+        </section>
+    )
+}
