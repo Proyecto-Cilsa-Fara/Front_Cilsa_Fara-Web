aa2795224249e394ae6d58174d37d9e001515c16
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-04-04 02:54:57 -0300

    Change AcercaDe.jsx <p> description according to client feedback

--- src/components/AcercaDe.jsx
+++ src/components/AcercaDe.jsx
@@ -3,7 +3,7 @@ import FARA_BANNER_IMAGE from "../assets/BANNER-2-1024x267.webp"
 export function AcercaDe() {
     return (
         <section className="bg-fara-cyan w-full p-8 text-center text-black lg:p-24">
-            <div className="flex h-full flex-col items-center justify-evenly gap-4 bg-white p-8 shadow-2xl">
+            <div className="flex h-full w-full flex-col items-center justify-evenly gap-4 bg-white p-8 shadow-2xl">
                 <img
                     src={FARA_BANNER_IMAGE}
                     alt="Logotipo de la Fundación Acción Restaurativa Argentina con bloques de color y texto"
@@ -11,15 +11,21 @@ export function AcercaDe() {
                     height="267px"
                     width="1024px"
                 />
-                <h1 className="text-2xl font-extrabold text-balance lg:text-4xl lg:font-bold">
+                <h1 className="w-full text-2xl font-extrabold text-balance lg:text-4xl lg:font-bold">
                     FARA – Fundación Acción Restaurativa Argentina
                 </h1>
                 <hr className="my-1 w-4/5 border-1 border-black" />
-                <p className="max-w-[75ch] text-xl text-balance lg:px-5">
-                    Asesoramos, acompañamos y desarrollamos programas basados en el paradigma restaurativo, promoviendo
-                    la resolución de conflictos y la reparación del daño. Capacitamos a operadores y profesionales para
-                    implementar procesos restaurativos, fomentando el diálogo, la reconciliación y el fortalecimiento de
-                    la comunidad.
+                <p className="max-w-[75ch] text-left text-xl lg:px-5">
+                    Asesoramos, acompañamos y desarrollamos programas basados en el{" "}
+                    <strong>paradigma restaurativo</strong>, promoviendo la resolución de conflictos y la reparación del
+                    daño. Capacitamos a operadores y profesionales para implementar procesos restaurativos, fomentando
+                    el <strong>diálogo</strong>, la <strong>reconciliación</strong> y el fortalecimiento de la
+                    comunidad. A través de <em>iniciativas restaurativas en las comunidades</em> —como la{" "}
+                    <strong>facilitación</strong>, los <strong>círculos restaurativos</strong> y las{" "}
+                    <strong>reuniones de familias</strong>— y un <em>sólido entrenamiento en justicia restaurativa</em>{" "}
+                    —incluyendo <strong>formación</strong>, <strong>apoyo</strong>, <strong>consultorías</strong> y{" "}
+                    <strong>orientación personalizada</strong>— ayudamos a construir espacios de justicia más humanos y
+                    colaborativos.
                 </p>
             </div>
         </section>
