c6e092db4ad13c260234d3cc9c11d4bf48b0591d
Matías Díaz <mati98ld@gmail.com>
2025-04-03 23:46:06 -0300

    Refactor: implement Layout

--- src/pages/InstitucionalPage.jsx
+++ src/pages/InstitucionalPage.jsx
@@ -1,48 +1,27 @@
 import {
-    ColorBar,
-    ContactSection,
     DonarSection,
     Areas,
-    NavBar,
-    DonarFAB,
     AcercaDeNosotros,
     SociosRestauradores,
     Legales,
     Integrantes,
+    Layout,
 } from "../components/index"
 
 export function InstitucionalPage() {
-    const FOOTER_MESSAGE = "© 2025 Acción Restaurativa Argentina. Todos los derechos reservados."
-
     return (
-        <div className="flex min-h-screen grow flex-col">
-            <header className="fixed z-10 flex w-full flex-col">
-                <ColorBar />
+        <Layout>
+            <SociosRestauradores />
 
-                <NavBar />
-            </header>
+            <AcercaDeNosotros />
 
-            <main className="mt-30 grid grow-[1] place-items-center">
-                <SociosRestauradores />
+            <Integrantes />
 
-                <AcercaDeNosotros />
+            <DonarSection />
 
-                <Integrantes />
+            <Areas />
 
-                <DonarSection />
-
-                <Areas />
-
-                <Legales />
-
-                <ContactSection />
-
-                <DonarFAB />
-            </main>
-            <footer className="w-full flex-col gap-4">
-                <p className="grid h-[calc(100%-1rem)] place-items-center p-4 text-sm">{FOOTER_MESSAGE}</p>
-                <ColorBar />
-            </footer>
-        </div>
+            <Legales />
+        </Layout>
     )
 }
