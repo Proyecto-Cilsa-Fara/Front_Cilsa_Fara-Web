ba17f6ab6d7659154b8cb72bc4a35c81d686c9e1
Matías Díaz <mati98ld@gmail.com>
2025-04-06 23:19:22 -0300

    Add component 'AreasDeIncidencia' in 'SociosPage'

--- src/pages/SociosPage.jsx
+++ src/pages/SociosPage.jsx
@@ -1,4 +1,4 @@
-import { AliadosSection, SociosRestauradores, Layout, Beneficios } from "../components/index"
+import { AliadosSection, SociosRestauradores, Layout, Beneficios, AreasDeIncidencia } from "../components/index"
 
 export function SociosPage() {
     return (
@@ -9,10 +9,7 @@ export function SociosPage() {
 
             <Beneficios />
 
-            {/* Equipos-Areas (Reemplazar por componente Equipos-Areas al div identificado)*/}
-            <div id="Equipos-Areas" className="bg-fara-gold w-full py-100 text-center text-3xl font-bold text-white">
-                <h2>Acá va el componente Equipos-Areas</h2>
-            </div>
+            <AreasDeIncidencia />
 
             {/* BannerSumate (Reemplazar por componente BannerSumate al div identificado)*/}
             <div id="BannerSumate" className="bg-fara-red w-full py-50 text-center text-3xl font-bold text-white">
