728905252ca8b8cc8c23f84d60978eb7fcaa52cf
Matías Díaz <mati98ld@gmail.com>
2025-04-09 12:56:13 -0300

    Estructura general de página 'Capacitaciones'

---
+++ src/pages/CapacitacionesPage.jsx
@@ -0,0 +1,14 @@
+import { Layout } from "../components/index"
+
+export function CapacitacionesPage() {
+    return (
+        <Layout>
+            <div className="w-full py-80 text-center">
+                <h2 className="text-3xl font-bold">Acá va el componente Hero</h2>
+            </div>
+            <div className="w-full bg-gray-200 py-100 text-center">
+                <h2 className="text-3xl font-bold">Acá va el componente Cursos</h2>
+            </div>
+        </Layout>
+    )
+}
