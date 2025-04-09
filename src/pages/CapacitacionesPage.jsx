3270edbb64069aa1d67f3411ed5198f07236327c
Matías Díaz <mati98ld@gmail.com>
2025-04-09 14:05:01 -0300

    Add component 'CapacitacionesHero' in CapacitacionesPage

--- src/pages/CapacitacionesPage.jsx
+++ src/pages/CapacitacionesPage.jsx
@@ -1,11 +1,10 @@
-import { Layout } from "../components/index"
+import { Layout, CapacitacionesHero } from "../components/index"
 
 export function CapacitacionesPage() {
     return (
         <Layout>
-            <div className="w-full py-80 text-center">
-                <h2 className="text-3xl font-bold">Acá va el componente Hero</h2>
-            </div>
+            <CapacitacionesHero />
+
             <div className="w-full bg-gray-200 py-100 text-center">
                 <h2 className="text-3xl font-bold">Acá va el componente Cursos</h2>
             </div>
