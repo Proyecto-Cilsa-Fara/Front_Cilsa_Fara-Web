28042b366f64eba0599a9c5885688f598f59de7a
Matías Díaz <mati98ld@gmail.com>
2025-04-09 21:38:59 -0300

    Add component 'GridCursos' in 'CapacitacionesPage'

--- src/pages/CapacitacionesPage.jsx
+++ src/pages/CapacitacionesPage.jsx
@@ -1,13 +1,11 @@
-import { Layout, CapacitacionesHero } from "../components/index"
+import { Layout, CapacitacionesHero, GridCursos } from "../components/index"
 
 export function CapacitacionesPage() {
     return (
         <Layout>
             <CapacitacionesHero />
 
-            <div className="w-full bg-gray-200 py-100 text-center">
-                <h2 className="text-3xl font-bold">Acá va el componente Cursos</h2>
-            </div>
+            <GridCursos />
         </Layout>
     )
 }
