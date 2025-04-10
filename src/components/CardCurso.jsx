aebc06e0cb2668454013e0e54c5369641e650a31
Matías Díaz <mati98ld@gmail.com>
2025-04-10 15:48:27 -0300

    Fix touchpad screens bug

--- src/components/CardCurso.jsx
+++ src/components/CardCurso.jsx
@@ -4,9 +4,9 @@ import CardCursoImage from "../assets/CardCursoImage.webp"
 
 export function CardCurso({ title, description, image = CardCursoImage, link }) {
     return (
-        <div className="bg-fara-teal group hover:bg-fara-lime focus:bg-fara-lime active:bg-fara-lime [&:has(a:hover)]:bg-fara-lime [&:has(a:focus)]:bg-fara-lime [&:has(a:active)]:bg-fara-lime -skew-y-6 px-4 py-2 shadow-2xl transition-colors duration-500 ease-in-out hover:skew-y-6 focus:skew-y-6 active:skew-y-6 [&:has(a:active)]:skew-y-6 [&:has(a:focus)]:skew-y-6 [&:has(a:hover)]:skew-y-6">
+        <div className="bg-fara-teal group pointer-coarse:skew-y-6 hover:bg-fara-lime focus:bg-fara-lime active:bg-fara-lime [&:has(a:hover)]:bg-fara-lime [&:has(a:focus)]:bg-fara-lime [&:has(a:active)]:bg-fara-lime -skew-y-6 cursor-pointer px-4 py-2 shadow-2xl transition-colors duration-500 ease-in-out hover:skew-y-6 focus:skew-y-6 active:skew-y-6 [&:has(a:active)]:skew-y-6 [&:has(a:focus)]:skew-y-6 [&:has(a:hover)]:skew-y-6">
             <Link
-                className="flex h-full w-full max-w-80 skew-y-6 flex-col items-center justify-between gap-6 bg-white shadow-xl group-hover:-skew-y-6 group-focus:-skew-y-6 group-active:-skew-y-6 hover:-skew-y-6 focus:-skew-y-6 active:-skew-y-6"
+                className="pointer-coarse:-skew-y-6 flex h-full w-full max-w-80 skew-y-6 cursor-pointer flex-col items-center justify-between gap-6 bg-white shadow-xl group-hover:-skew-y-6 group-focus:-skew-y-6 group-active:-skew-y-6 hover:-skew-y-6 focus:-skew-y-6 active:-skew-y-6"
                 to={link}
                 aria-label={`Anotarse al curso ${title}`}
             >
