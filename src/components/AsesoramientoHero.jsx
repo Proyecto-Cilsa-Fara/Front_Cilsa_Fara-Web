b82ee4788fe491143a67b47571addba89cc38086
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-04-11 06:35:48 -0300

    Fix styling in AsesoramientoHero.jsx

--- src/components/AsesoramientoHero.jsx
+++ src/components/AsesoramientoHero.jsx
@@ -6,14 +6,16 @@ export function AsesoramientoHero() {
             <div className="relative">
                 <img
                     src={AsesoramientoHeroImage}
-                    className="w-full object-cover lg:h-150 xl:h-170"
+                    className="w-full scale-x-[-1] object-cover lg:h-150 xl:h-170"
                     height="700px"
                     alt="Imagen de una persona mirando una computadora"
                 />
                 <div className="bg-fara-red/85 absolute bottom-0 flex h-1/2 w-full items-center justify-center text-center">
-                    <h2 className="text-xl font-bold text-white sm:text-2xl md:text-4xl xl:text-6xl">
-                        Servicios para Organizaciones
-                    </h2>
+                    <div className="mx-auto flex h-1/3 w-5/6 -skew-x-12 items-center justify-center bg-white text-center">
+                        <h2 className="skew-x-12 text-xl font-bold sm:text-2xl md:text-4xl xl:text-6xl">
+                            Servicios para Organizaciones
+                        </h2>
+                    </div>
                 </div>
             </div>
             <div className="skew-custom bg-fara-blue w-5/6 -translate-y-5 scale-x-[-1] justify-self-end p-3 text-center sm:p-10 md:-translate-y-10 lg:w-3/4">
