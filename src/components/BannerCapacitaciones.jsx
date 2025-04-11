959433f6d834e843c54ac43d3d1e36b02d4805f0
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-04-11 05:35:56 -0300

    Fix className conflict in BannerCapacitaciones.jsx

--- src/components/BannerCapacitaciones.jsx
+++ src/components/BannerCapacitaciones.jsx
@@ -9,7 +9,7 @@ export function BannerCapacitaciones() {
             <Link
                 to="/capacitaciones"
                 aria-label="Mirá todas nuestras capacitaciones"
-                className="skew-custom bg-fara-blue hover:[.text-outline-lg] hover:bg-fara-red focus:bg-fara-red active:bg-fara-red mx-auto grid min-h-12 w-5/6 min-w-48 place-items-center border-b-2 border-gray-200 pr-8 pl-6 text-base font-bold uppercase transition-colors duration-300 ease-in-out hover:text-white focus:text-white active:text-white md:w-1/2 md:text-3xl md:text-xl xl:w-1/3"
+                className="skew-custom bg-fara-blue hover:[.text-outline-lg] hover:bg-fara-red focus:bg-fara-red active:bg-fara-red mx-auto grid min-h-12 w-5/6 min-w-48 place-items-center border-b-2 border-gray-200 pr-8 pl-6 text-base font-bold uppercase transition-colors duration-300 ease-in-out hover:text-white focus:text-white active:text-white md:w-1/2 md:text-3xl xl:w-1/3"
             >
                 Ver capacitaciones
             </Link>
