495f8f1f8259e382ea81009c3427c3a2ed4ff0cc
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-04-11 06:23:53 -0300

    Add new logos to AliadosSection.jsx

--- src/assets/aliados-fara/index.js
+++ src/assets/aliados-fara/index.js
@@ -2,8 +2,13 @@ import AtalycImage from "./ATALyC.webp"
 import BuenosAiresImage from "./Buenos-Aires.webp"
 import ComnapazMexicoImage from "./Comnapaz-Mexico.webp"
 import FundacionGuillermoSnopekImage from "./Fundacion-Guillermo-Snopek.webp"
+import IIBJImage from "./IIBJ.webp"
+import IMMImage from "./IMM.webp"
 import InceipImage from "./INCEIP.webp"
+import JusticiaRestaurativaImage from "./JusticiaRestaurativa.webp"
+import MedianteImage from "./Mediante.webp"
 import MinisterioPublicoDeLaDefensaImage from "./Ministerio-Publico-de-la-defensa.webp"
+import Objetivo16Image from "./Objetivo16.webp"
 import RockefellerCenterImage from "./Rockefeller-Center.webp"
 import ServicioPenitenciarioBonaerenseImage from "./Servicio-Penitenciario-Bonaerense.webp"
 import SipinnaImage from "./SIPINNA.webp"
@@ -16,8 +21,13 @@ const aliadosImages = [
     FundacionGuillermoSnopekImage,
     MinisterioPublicoDeLaDefensaImage,
     InceipImage,
+    IMMImage,
+    MedianteImage,
     AtalycImage,
+    IIBJImage,
+    JusticiaRestaurativaImage,
     SipinnaImage,
+    Objetivo16Image,
 ]
 
 export { aliadosImages }
