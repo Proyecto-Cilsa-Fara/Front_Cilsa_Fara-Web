102e045dedadc32e2f3b4d268c2cd5acf79f4fc0
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-04-11 07:02:43 -0300

    Update images in Integrantes.jsx

--- src/assets/integrantes/index.js
+++ src/assets/integrantes/index.js
@@ -1,69 +1,46 @@
-import CecilSimonuttiIMG from "./Cecil-Simonutti.webp"
-import LucasBongoviIMG from "./Lucas-Bongovi.webp"
-import MatiasMazzaIMG from "./Matias-Mazza.webp"
-import NatalieNoechwiczIMG from "./Natalie-Noechwicz.webp"
-import SilvanaPazIMG from "./Silvana-Paz.webp"
+import AlejandraBarranqueroImage from "./AlejandraBarranquero.webp"
+import CintiaImage from "./Cintia.webp"
+import ConstanzaImage from "./Constanza.webp"
+import SantiagoCherryImage from "./SantiagoCherry.webp"
+import SilvanaPazImage from "./SilvanaPaz.webp"
+import SilvinaPazImage from "./SilvinaPaz.webp"
 
 const INTEGRANTES = [
     {
-        name: "Silvana Paz",
+        name: "Silvina Paz",
         position: "Fundadora",
-        photo: SilvanaPazIMG,
+        photo: SilvinaPazImage,
         color: "bg-fara-teal",
-    },
-    {
-        name: "Cecil Simonutti",
-        position: "Tesorera",
-        photo: CecilSimonuttiIMG,
-        color: "bg-fara-lime",
-    },
-    {
-        name: "Lucas Bongovi",
-        position: "Tec. Informática",
-        photo: LucasBongoviIMG,
-        color: "bg-fara-gold",
-    },
-    {
-        name: "Matías Mazza",
-        position: "Redes",
-        photo: MatiasMazzaIMG,
-        color: "bg-fara-red",
-    },
-    {
-        name: "Natalie Noechwicz",
-        position: "Corresponsal Uruguay",
-        photo: NatalieNoechwiczIMG,
-        color: "bg-fara-cyan",
     },
     {
         name: "Silvana Paz",
         position: "Fundadora",
-        photo: SilvanaPazIMG,
-        color: "bg-fara-teal",
-    },
-    {
-        name: "Cecil Simonutti",
-        position: "Tesorera",
-        photo: CecilSimonuttiIMG,
+        photo: SilvanaPazImage,
         color: "bg-fara-lime",
     },
     {
-        name: "Lucas Bongovi",
-        position: "Tec. Informática",
-        photo: LucasBongoviIMG,
+        name: "Alejandra Barranquero",
+        position: "Presidenta",
+        photo: AlejandraBarranqueroImage,
         color: "bg-fara-gold",
     },
     {
-        name: "Matías Mazza",
-        position: "Redes",
-        photo: MatiasMazzaIMG,
+        name: "Constanza Ochoa",
+        position: "Secretaria",
+        photo: ConstanzaImage,
         color: "bg-fara-red",
     },
     {
-        name: "Natalie Noechwicz",
-        position: "Corresponsal Uruguay",
-        photo: NatalieNoechwiczIMG,
+        name: "Santiago Cherry",
+        position: "Tesorero",
+        photo: SantiagoCherryImage,
         color: "bg-fara-cyan",
+    },
+    {
+        name: "Cintia",
+        position: "Prosecretaria",
+        photo: CintiaImage,
+        color: "bg-fara-teal",
     },
 ]
 
