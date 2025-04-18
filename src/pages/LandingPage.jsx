80e7a678e27ab0451ac9ca52e15db9d1911dc92b
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-04-18 01:09:28 -0300

    Add local link to ContactSection in LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,10 +3,23 @@ import { useLocation } from "react-router"
 import { AcercaDe, ServiciosSection, DonarSection, AliadosSection, Areas, Carrousel, Layout } from "../components/index"
 
 export function LandingPage() {
-    const { pathname } = useLocation()
+    const { pathname, hash } = useLocation()
+
     useEffect(() => {
         window.scrollTo(0, 0)
     }, [pathname])
+
+    useEffect(() => {
+        if (hash) {
+            const element = document.getElementById(hash.replace("#", ""))
+            if (element) {
+                element.scrollIntoView()
+                // Si bien se puede usar { behavior: "smooth" } para dar una animación
+                // lamentablemente no es accesible por la falta de control nativo que
+                // nos permita ajustarlo acorde a los estándares de accesibilidad.
+            }
+        }
+    }, [hash])
 
     return (
         <Layout>
