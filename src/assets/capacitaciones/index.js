72c1790e316ca2e15aadea6d307ea8ed72c01993
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-04-26 14:30:56 -0300

    Add capacitaciones images for all items in GridCursos.jsx

--- src/assets/capacitaciones/index.js
+++ src/assets/capacitaciones/index.js
@@ -1,4 +1,8 @@
 import CardCursoImage from "./CardCursoImage.webp"
 import DiplomaturaImage from "./Diplomatura.webp"
+import EncierroImage from "./Encierro.webp"
+import EscolarImage from "./Escolar.webp"
+import GeneroImage from "./Genero.webp"
+import JuventudImage from "./Juventud.webp"
 
-export { CardCursoImage, DiplomaturaImage }
+export { CardCursoImage, DiplomaturaImage, EncierroImage, EscolarImage, GeneroImage, JuventudImage }
