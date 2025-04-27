f3b81a7dac80cea5a47b865d29b37af55d56fe1a
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-04-27 13:30:25 -0300

    Add CarrouselDonar images in its own assets directory

---
+++ src/assets/carrousel-donar/index.js
@@ -0,0 +1,6 @@
+import DiplomaturaImage from "./Diplomatura.webp"
+import EncierroImage from "./Encierro.webp"
+import EscolarImage from "./Escolar.webp"
+import VoluntariadosImage from "./VoluntariadosImage.webp"
+
+export { DiplomaturaImage, EncierroImage, EscolarImage, VoluntariadosImage }
