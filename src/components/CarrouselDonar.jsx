41e0120cc2e48ab832e702c928a496a958a47f7a
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2025-04-27 17:53:30 -0300

    Fix responsive design issues in CarrouselDonar.jsx

--- src/components/CarrouselDonar.jsx
+++ src/components/CarrouselDonar.jsx
@@ -33,7 +33,7 @@ export function CarrouselDonar() {
 
         intervalRef.current = setInterval(() => {
             setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length)
-        }, 12000)
+        }, 90000)
     }, [])
 
     const prevSlide = useCallback(() => {
@@ -78,23 +78,23 @@ export function CarrouselDonar() {
                     style={{ transform: `translateX(-${currentIndex * 100}%)` }}
                 >
                     {slides.map((slide, index) => (
-                        <li key={index} className="relative h-140 w-full flex-shrink-0 bg-[#555555] bg-cover bg-center">
+                        <li key={index} className="bg-fara-gray relative h-140 w-full flex-shrink-0 bg-cover bg-center">
                             <figure className="h-full w-full">
                                 {/* Contenido */}
-                                <figcaption className="absolute top-1/2 left-1/2 flex w-2/3 -translate-x-1/2 -translate-y-1/2 flex-col gap-2 md:gap-8">
-                                    <div className="mx-auto flex h-full w-full max-w-200 -skew-x-5 items-center justify-center gap-4 bg-white p-4 text-center md:flex-row md:p-6 lg:max-w-400 lg:p-16">
+                                <figcaption className="absolute top-1/2 left-1/2 flex h-4/5 w-2/3 -translate-x-1/2 -translate-y-1/2 flex-col gap-2 md:gap-8">
+                                    <div className="mx-auto flex h-full w-full -skew-x-2 flex-col items-center justify-center gap-4 bg-white p-8 text-center md:max-w-400 md:-skew-x-5 md:flex-row md:gap-8 md:p-6 lg:p-16">
                                         <img
                                             src={slide.image}
                                             alt={slide.tittle}
-                                            className="aspect-3/2 w-1/2 skew-x-5 overflow-hidden rounded-2xl object-cover object-top lg:w-1/3"
+                                            className="aspect-3/2 w-80 overflow-hidden rounded-2xl object-cover object-top md:skew-x-5 lg:w-1/3"
                                             width="1024px"
                                             height="400px"
                                         />
-                                        <div className="flex w-1/2 skew-x-5 flex-col justify-center gap-4 p-2 lg:w-2/3">
-                                            <h3 className="flex min-h-[2rlh] w-full items-center text-lg font-bold uppercase md:text-xl lg:text-2xl xl:text-4xl">
+                                        <div className="flex w-full skew-x-2 flex-col justify-center gap-4 md:skew-x-5 md:gap-8 md:p-2 lg:w-2/3">
+                                            <h3 className="flex w-full text-left text-lg font-bold uppercase md:min-h-[2rlh] md:text-xl lg:text-2xl xl:text-4xl">
                                                 {slide.tittle}
                                             </h3>
-                                            <p className="flex min-h-[5rlh] w-full items-center text-left text-xs text-balance sm:text-sm md:text-lg xl:text-2xl">
+                                            <p className="flex w-full skew-x-2 items-center text-left text-xs text-balance sm:text-sm md:min-h-[5rlh] md:text-lg xl:text-2xl">
                                                 {slide.text}
                                             </p>
                                         </div>
