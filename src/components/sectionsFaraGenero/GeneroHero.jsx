1c392af5b903facd945c485638ce7edb2045a286
papulodev <papulodev@gmail.com>
2026-08-28 12:41:04 -0300

    feat(genero): enhance GeneroHero component with email contact functionality and improved structure

--- src/components/sectionsFaraGenero/GeneroHero.jsx
+++ src/components/sectionsFaraGenero/GeneroHero.jsx
@@ -1,37 +1,44 @@
-import { Link } from "react-router";
-import { useDocumentMeta } from "../../hooks/useDocumentMeta";
+import { Link } from "react-router"
+import { useDocumentMeta } from "../../hooks/useDocumentMeta"
+import { useEmailContact } from "../../hooks/useEmailContact"
 
 export function GeneroHero() {
+    const TITLE = "Género y Diversidad"
+    const DESCRIPTION =
+        "Acompañamos, formamos y transformamos desde una mirada que reconoce el daño, cuida a las personas y exige responsabilidad."
+    useDocumentMeta(TITLE, DESCRIPTION)
 
-  const TITLE = "Género y Diversidad"
-  const DESCRIPTION =
-    "Acompañamos, formamos y transformamos desde una mirada que reconoce el daño, cuida a las personas y exige responsabilidad."
-  useDocumentMeta(TITLE, DESCRIPTION);
+    const { onClikEmail, mailtoEmail } = useEmailContact("outlook", "Solicitud de asesoramiento de Fara Género")
 
-  return (
-    <section className="w-full">
-      <div className="bg-genero-image relative flex h-dvh w-full flex-col items-center justify-center gap-4 md:h-80 lg:h-150 xl:h-170">
-        <div className="flex flex-col h-full w-full items-center justify-center gap-7 bg-black/50 text-white">
-          <h1 className="px-4 text-5xl font-bold md:text-4xl xl:text-6xl max-w-4xl md:text-center">{TITLE}</h1>
-          <p className="text-base text-start px-4 md:text-lg xl:text-xl md:text-center text-wrap max-w-3xl">{DESCRIPTION}</p>
-          <div className="flex flex-col items-start gap-4 w-full px-4 md:flex-row md:justify-center">
-            <Link
-              to="/capacitaciones"
-              className="skew-custom bg-fara-blue hover:bg-fara-cyan active:bg-fara-cyan focus:bg-fara-cyan text-center px-4 py-3 text-white uppercase shadow-2xl transition-colors duration-300 ease-in-out hover:cursor-pointer font-bold"
-              aria-label="Solicitar Acompañamiento"
-            >
-              Solicitar Acompañamiento
-            </Link>
-            <Link
-              to="/capacitaciones"
-              className="skew-custom bg-fara-red active:bg-fara-strong-red hover:bg-fara-strong-red focus:bg-fara-strong-red text-center px-4 py-3 text-white uppercase shadow-2xl transition-colors duration-300 ease-in-out hover:cursor-pointer font-bold"
-              aria-label="Pedir Capacitación"
-            >
-              Pedir Capacitación
-            </Link>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+    return (
+        <section className="w-full">
+            <div className="bg-genero-image relative flex h-dvh w-full flex-col items-center justify-center gap-4 md:h-80 lg:h-150 xl:h-170">
+                <div className="flex h-full w-full flex-col items-center justify-center gap-7 bg-black/50 text-white">
+                    <h1 className="max-w-4xl px-4 text-5xl font-bold md:text-center md:text-4xl xl:text-6xl">
+                        {TITLE}
+                    </h1>
+                    <p className="max-w-3xl px-4 text-start text-base text-wrap md:text-center md:text-lg xl:text-xl">
+                        {DESCRIPTION}
+                    </p>
+                    <div className="flex w-full flex-col items-start gap-4 px-4 md:flex-row md:justify-center">
+                        <a
+                            href={mailtoEmail}
+                            onClick={onClikEmail}
+                            className="skew-custom bg-fara-blue hover:bg-fara-cyan active:bg-fara-cyan focus:bg-fara-cyan px-4 py-3 text-center font-bold text-white uppercase shadow-2xl transition-colors duration-300 ease-in-out hover:cursor-pointer"
+                            aria-label="Solicitar asesoramiento"
+                        >
+                            Solicitar asesoramiento
+                        </a>
+                        <Link
+                            to="/capacitaciones"
+                            className="skew-custom bg-fara-red active:bg-fara-strong-red hover:bg-fara-strong-red focus:bg-fara-strong-red px-4 py-3 text-center font-bold text-white uppercase shadow-2xl transition-colors duration-300 ease-in-out hover:cursor-pointer"
+                            aria-label="Pedir Capacitación"
+                        >
+                            Pedir Capacitación
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
