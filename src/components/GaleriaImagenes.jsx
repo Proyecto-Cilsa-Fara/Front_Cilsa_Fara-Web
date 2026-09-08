aea184b99ee9477f66740a2e18307cb08a78721d
papulodev <papulodev@gmail.com>
2026-09-07 22:52:18 -0300

    update(agenda): add new event images and update gallery structure

--- src/components/GaleriaImagenes.jsx
+++ src/components/GaleriaImagenes.jsx
@@ -14,7 +14,7 @@ export function GaleriaImagenes({ photosGallery }) {
     useEffect(() => {
         let lightbox = new PhotoSwipeLightbox({
             gallery: "#grid-gallery-fara",
-            children: "a",
+            children: "a[id='item-image']",
             pswpModule: () => import("photoswipe"),
         })
         lightbox.init()
@@ -36,26 +36,44 @@ export function GaleriaImagenes({ photosGallery }) {
                 {photos.map((img, i) => {
                     const description = getImgDescription(img.src)
                     return (
-                        <a
-                            href={img.src}
-                            key={"grid-gallery-fara" + "-" + i}
-                            data-pswp-width={img.width}
-                            data-pswp-height={img.height}
-                            target="_blank"
-                            rel="noreferrer"
-                            className="break-inside-avoid overflow-hidden"
-                            aria-label={description}
-                        >
-                            <img
-                                className="mb-4 w-full object-cover transition-transform duration-300 hover:scale-105"
-                                src={img.src}
-                                alt={description}
-                                loading="lazy"
-                                width={img.width}
-                                height={img.height}
-                                style={{ aspectRatio: `${img.width} / ${img.height}` }}
-                            />
-                        </a>
+                        <div key={"gallery-fara" + "-" + i} className="relative">
+                            <a
+                                href={img.src}
+                                role="link"
+                                id="item-image"
+                                data-pswp-width={img.width}
+                                data-pswp-height={img.height}
+                                target="_blank"
+                                rel="noreferrer"
+                                className="break-inside-avoid overflow-hidden"
+                                aria-label={description}
+                            >
+                                <img
+                                    className="mb-4 w-full object-cover transition-transform duration-300 hover:scale-105"
+                                    src={img.src}
+                                    alt={description}
+                                    loading="lazy"
+                                    width={img.width}
+                                    height={img.height}
+                                    style={{ aspectRatio: `${img.width} / ${img.height}` }}
+                                />
+
+                                <div className="absolute inset-0 flex items-center justify-center bg-black/50 bg-opacity-50 text-white opacity-0 font-bold transition-opacity duration-300 hover:opacity-100">
+                                    Ver imagen
+                                </div>
+                            </a>
+                            {img.link && (
+                                <a
+                                    href={img.link}
+                                    target="_blank"
+                                    id="item-link"
+                                    rel="noreferrer"
+                                    className="absolute bottom-0 left-0 flex items-center justify-center bg-fara-strong-red text-white font-bold py-3 px-2 rounded-md w-full"
+                                >
+                                    Abrir enlace
+                                </a>
+                            )}
+                        </div>
                     )
                 })}
             </div>
