49be4783be840f37cb3716d78bfa1f33c79e2f32
papulodev <papulodev@gmail.com>
2026-09-11 20:12:55 -0300

    fix(contactInfo): update YouTube handle and link for consistency

--- src/utils/contactInfo.js
+++ src/utils/contactInfo.js
@@ -20,6 +20,6 @@ export const CONTACT_INFO = {
     LINKEDIN: "Acción Restaurativa Argentina",
     LINKEDIN_LINK: "https://www.linkedin.com/in/acci%C3%B3n-restaurativa-argentina-42b011214/",
 
-    YOUTUBE: "@FARA-ARGENTINA",
-    YOUTUBE_LINK: "https://www.youtube.com/@fara-argentina",
+    YOUTUBE: "@ACCIÓNRESTAURATIVAARGENTINA",
+    YOUTUBE_LINK: "https://www.youtube.com/@ACCIONRESTAURATIVAARGENTINA",
 }
